701fe1351de5e3726bc84c65f457523c854aaf10
ssc74 <113689815+ssc74@users.noreply.github.com>
2022-10-21 20:50:49 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,8 @@
+function filter_list(arr)
+{  
+    return arr.filter(a => typeof a == 'number')  
+}  
+  
+console.log(filter_list([1,2,'a','b'])) // [1,2]   
+console.log(filter_list([1,'a','b',0,15])) // [1,0,15]   
+console.log(filter_list([1,2,'aasf','1','123',123])) // [1,2,123]
